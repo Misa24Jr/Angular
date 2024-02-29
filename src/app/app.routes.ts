3e083bd5f55e5a591ce4b84562b1dd85a9d9ab56
Misa24jr <misa24jr@gmail.com>
2024-02-28 21:58:43 -0400

    Primeros cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { ButtonComponent } from './button/button.component';
+import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
+// import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+   {
+    path: 'button', 
+    component: ButtonComponent
+   },
+   {
+    path: 'input',
+    component: InputComponent
+   },
+   {
+    path: 'card',
+    component: CardComponent
+   },
+//    {
+//     path: '',
+//     redirectTo: '/home',
+//     component: HomeComponent,
+//     pathMatch: 'full'
+//    }
+];
